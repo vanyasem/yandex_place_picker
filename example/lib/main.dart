418334610893ed2161a84432a520faedbc99c216
Ivan Semkin <ivan@semkin.ru>
2019-08-15 18:10:59 +0300

    Flutter: Rename to yandex_place_picker

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:yandex_placepicker/yandexplacepicker.dart';
+import 'package:yandex_place_picker/yandexplacepicker.dart';
 
 void main() => runApp(MyApp());
 
