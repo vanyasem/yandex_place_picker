d223ae390b2cdd6465be72083ded3571b5ec8e14
Ivan Semkin <ivan@semkin.ru>
2019-08-19 15:17:10 +0300

    Flutter: Lint

--- lib/yandexplacepicker.dart
+++ lib/yandexplacepicker.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class YandexPlacePicker {
-  static const MethodChannel _channel =
-      const MethodChannel('yandexplacepicker');
+  static const MethodChannel _channel = MethodChannel('yandexplacepicker');
 
   static dynamic get launchPicker async {
     final dynamic result = await _channel.invokeMethod('launch_picker');
