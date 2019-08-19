4c774539e4edf1528c55ad9662b409ded5d49c61
Ivan Semkin <ivan@semkin.ru>
2019-08-19 15:56:36 +0300

    Flutter: Delete Native code

--- ios/Classes/YandexPlacePickerPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface YandexPlacePickerPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/YandexPlacePickerPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "YandexPlacePickerPlugin.h"
-#import <yandex_place_picker/yandex_place_picker-Swift.h>
-
-@implementation YandexPlacePickerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftYandexPlacePickerPlugin registerWithRegistrar:registrar];
-}
-@end
