d3c5e17565ddfd9306d7199391c57e2fa63e41cc
Ivan Semkin <ivan@semkin.ru>
2019-08-20 21:07:27 +0300

    Flutter: Add placeholder placemark icons

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,3 +1,5 @@
 class ImageAssets {
   static const String userLocation = 'assets/user_location.png';
+
+  static const String placeDefault = 'assets/place_default.png';
 }
---
+++ lib/src/utils.dart
@@ -0,0 +1,9 @@
+import 'package:yandex_place_picker/assets.dart';
+
+class Utils {
+
+  static String getPlaceIconName() {
+    // TODO: Implement Icons
+    return ImageAssets.placeDefault;
+  }
+}
